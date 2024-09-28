dec523a4f4c088ea3c5bcee31898a2e8a619253c
PabitraBhandari <89492267+PabitraBhandari@users.noreply.github.com>
2024-09-28 11:28:13 -0500

    Update memorymnmt.py

--- PHASE3/memorymnmt.py
+++ PHASE3/memorymnmt.py
@@ -1,3 +1,14 @@
+""" 
+This code defines a simple memory pool system for managing Node objects. 
+The Node class represents a doubly linked list node with license_plate, 
+next, and prev attributes. The MemoryPool class maintains a pool of 
+reusable Node objects. The get_node method returns a node from the pool 
+if available, or creates a new one if the pool is empty. The release_node 
+method resets the node’s attributes and returns it to the pool for future 
+reuse, thus optimizing memory management by reusing nodes instead of 
+frequently creating and destroying them. 
+"""
+
 class Node:
     def __init__(self):
         self.license_plate = None
