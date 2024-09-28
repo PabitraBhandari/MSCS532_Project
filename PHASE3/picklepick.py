e070e82aca66d5264f42be4ec8ef9c99908f88fa
PabitraBhandari <89492267+PabitraBhandari@users.noreply.github.com>
2024-09-28 11:30:12 -0500

    Update picklepick.py

--- PHASE3/picklepick.py
+++ PHASE3/picklepick.py
@@ -1,3 +1,12 @@
+""" 
+This code provides two functions for serializing and deserializing vehicle data in a parking 
+lot system using Python's pickle module. The serialize_vehicle_data function saves the current 
+state of the vehicle data (stored in the vehicles attribute of a ParkingLot instance) to a file 
+named "vehicle_data.pkl". The deserialize_vehicle_data function reads from this file and restores 
+the vehicle data, returning the deserialized object. This allows for persisting the vehicle data 
+to disk and reloading it later. 
+"""
+
 import pickle
 
 def serialize_vehicle_data(parking_lot, filename="vehicle_data.pkl"):
